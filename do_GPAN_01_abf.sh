1b4a1cd97d940646ea02019b9bc31856b8ae1e8c
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2013-08-12 10:19:29 +0300

    Add shell file for 01

---
+++ do_GPAN_01_abf.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+zig4 -r GPAN_01_abf/GPAN_01_abf.dat GPAN_01_abf/GPAN_01_abf.spp GPAN_01_abf/output/GPAN_01_abf.txt 0.0 0 1.0 0 --grid-output-formats=compressed-tif --image-output-formats=png 

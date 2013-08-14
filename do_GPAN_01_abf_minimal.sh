438f69844de2eb56ed65d8a635905231403c5b17
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2013-08-14 11:15:20 +0300

    Add minimal test variant GPAN_01_abf_minimal

---
+++ do_GPAN_01_abf_minimal.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+zig4 -r GPAN_01_abf_minimal/GPAN_01_abf_minimal.dat GPAN_01_abf_minimal/GPAN_01_abf_minimal.spp GPAN_01_abf_minimal/output/GPAN_01_abf_minimal.txt 0.0 0 1.0 0 --grid-output-formats=compressed-tif --image-output-formats=png 

bfbe96c07720e70167f70e968f46898a2f23ef1b
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2013-08-15 12:44:30 +0300

    Update zig3.exe to zig4.exe in bats

--- do_GPAN_01_abf_minimal.bat
+++ do_GPAN_01_abf_minimal.bat
@@ -1,1 +1,1 @@
-call zig3.exe -r GPAN_01_abf_minimal/GPAN_01_abf_minimal.dat GPAN_01_abf_minimal/GPAN_01_abf_minimal.spp GPAN_01_abf_minimal/output/GPAN_01_abf_minimal.txt 0.0 0 1.0 0 --grid-output-formats=compressed-tif --image-output-formats=png 
+call zig4.exe -r GPAN_01_abf_minimal/GPAN_01_abf_minimal.dat GPAN_01_abf_minimal/GPAN_01_abf_minimal.spp GPAN_01_abf_minimal/output/GPAN_01_abf_minimal.txt 0.0 0 1.0 0 --grid-output-formats=compressed-tif --image-output-formats=png 

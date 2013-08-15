90bb46b1ed49f12b5e3504f09af5f8b8adcb5352
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2013-08-15 12:46:36 +0300

    Update all bats to use zig4.exe
    Previously all bat-file calls were using older zig3.exe

--- do_GPAN_08_caz_w_ecor_w40.bat
+++ do_GPAN_08_caz_w_ecor_w40.bat
@@ -1,1 +1,1 @@
-call zig3.exe -r GPAN_08_caz_w_ecor_w40/GPAN_08_caz_w_ecor_w40.dat GPAN_08_caz_w_ecor_w40/GPAN_08_caz_w_ecor_w40.spp GPAN_08_caz_w_ecor_w40/output/GPAN_08_caz_w_ecor_w40.txt 0.0 0 1.0 0 --grid-output-formats=compressed-tif --image-output-formats=png 
+call zig4.exe -r GPAN_08_caz_w_ecor_w40/GPAN_08_caz_w_ecor_w40.dat GPAN_08_caz_w_ecor_w40/GPAN_08_caz_w_ecor_w40.spp GPAN_08_caz_w_ecor_w40/output/GPAN_08_caz_w_ecor_w40.txt 0.0 0 1.0 0 --grid-output-formats=compressed-tif --image-output-formats=png 

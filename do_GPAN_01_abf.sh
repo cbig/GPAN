f27661719581eb3429d2f11e3508a7be883f8645
PKullberg <peter.kullberg@helsinki.fi>
2013-10-09 19:45:53 +0300

    remove marine mammals from spp lists
    Some uppdates also to groups and ADMU files

--- do_GPAN_01_abf.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-zig4 -r GPAN_01_abf/GPAN_01_abf.dat GPAN_01_abf/GPAN_01_abf.spp GPAN_output/GPAN_01_abf/GPAN_01_abf.txt 0.0 0 1.0 0 --grid-output-formats=compressed-tif --image-output-formats=png 
--- do_GPAN_01_abf_all_but_birds.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-zig4 -r GPAN_01_abf/GPAN_01_abf.dat GPAN_01_abf/GPAN_01_abf_all_but_birds.spp GPAN_output/GPAN_01_abf/GPAN_01_abf_all_but_birds.txt 0.0 0 1.0 0 --grid-output-formats=compressed-tif --image-output-formats=png 
